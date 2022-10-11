1c3f7d51bd8b153aae96264c9143b147ef7ff816
svdenbo1 <simon.vandenbossche@vdab.be>
2022-10-11 17:37:03 +0200

    extra logging to method

--- src/main/java/com/refresh/bean/refreshbean/controller/RefreshBeanController.java
+++ src/main/java/com/refresh/bean/refreshbean/controller/RefreshBeanController.java
@@ -2,6 +2,8 @@ package com.refresh.bean.refreshbean.controller;
 
 import com.refresh.bean.refreshbean.service.RefreshBeanService;
 import com.refresh.bean.refreshbean.service.domain.Boontje;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/bean")
 public class RefreshBeanController {
+    Logger logger = LoggerFactory.getLogger(RefreshBeanController.class);
+
     public final RefreshBeanService refreshBeanService;
     public final ApplicationContext context;
 
@@ -22,13 +26,17 @@ public class RefreshBeanController {
     @GetMapping("/refresh")
     public ResponseEntity<Void> refreshBean() {
         Boontje refreshBoontje = (Boontje) context.getBean("refreshBoontje");
-        System.out.println("Application context is geladen met: " + refreshBoontje + " en heeft als property: " + refreshBoontje.getProperty());
+        logRefreshBoontje(refreshBoontje);
 
         refreshBeanService.refreshBean();
 
         refreshBoontje = (Boontje) context.getBean("refreshBoontje");
-        System.out.println("Application context is geladen met: " + refreshBoontje + " en heeft als property: " + refreshBoontje.getProperty());
+        logRefreshBoontje(refreshBoontje);
 
         return ResponseEntity.ok().build();
+    }
+
+    private void logRefreshBoontje(Boontje refreshBoontje) {
+        logger.warn("Application context is geladen met: {} en heeft als property: {}", refreshBoontje, refreshBoontje.getProperty());
     }
 }
